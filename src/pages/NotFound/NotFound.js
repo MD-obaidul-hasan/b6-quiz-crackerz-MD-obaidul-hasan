48acd24935724f5f41f3f2868f743eb242aadc46
Mahfuz Moyeen <96814940+mahfuz-moyeen@users.noreply.github.com>
2022-10-12 02:35:30 +0600

    added notFound page

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div>
-            NotFound
+        <div className=' min-h-screen flex flex-col justify-center items-center'>
+            <h1 className='text-9xl text-red-500'>404</h1>
+            <p className='text-2xl mb-10'>Not Found Page</p>
+            <Link to='/' className='btn btn-link'>Go back to homepage</Link>
         </div>
     );
 };
