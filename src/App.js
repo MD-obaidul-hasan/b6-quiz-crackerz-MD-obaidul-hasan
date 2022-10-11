93c4656b9489c7c37cb7bcdbb78db0717a8e3cad
Mahfuz Moyeen <96814940+mahfuz-moyeen@users.noreply.github.com>
2022-10-11 22:45:27 +0600

    router setup and create page components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Main from './layouts/Main';
+import Home from './pages/Home/Home';
+import NotFound from './pages/NotFound/NotFound';
+import Quiz from './pages/Quiz/Quiz';
 
 function App() {
-  return (
-    <div >
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          loader: () => fetch('tshirts.json'),
+          element: <Home />
+        },
+        {
+          path: '/quiz/:id',
+          element: <Quiz />
+        },
+      ]
+    },
+    { path: '*', element: <NotFound /> },
+  ])
 
+
+  return (
+    <div>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/pages/Quiz/Quiz.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Quiz = () => {
+
+    return (
+        <div>
+            quiz
+        </div>
+    );
+};
+
+export default Quiz;
